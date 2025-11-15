5f41fb4ecae571439dd338b27ff621e8056476ef
Philip Kelsey <tinman@Mac.lan>
2025-11-15 17:03:00 -0500

    Revert: remove LifeLinkLog.png from header and restore previous header style

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
   const [page, setPage] = useState('home');
